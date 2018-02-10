7a0438739f34bfb0de37aeefdcbe75ae7af6554d
rakeshgirase <girase.rakesh@gmail.com>
2018-02-10 14:45:04 +0000

    QSB - Moved the Page Number to Action Bar

--- app/shared/result/result-view-model.ts
+++ app/shared/result/result-view-model.ts
@@ -12,6 +12,26 @@ export class ResultViewModel extends Observable {
         super();
         this._state = state;
         this.calculateResult();
+        this.initData();
+    }
+
+    private initData() {
+        this.set("pieSource",
+            [
+                { Brand: "Audi", Amount: 10 },
+                { Brand: "Mercedes", Amount: 76 },
+                { Brand: "Fiat", Amount: 60 },
+                { Brand: "BMW", Amount: 24 },
+                { Brand: "Crysler", Amount: 40 }
+            ]);
+
+        this.set("sourceItems", [
+            { Name: "Groceries", Sales: 25, Margin: 10 },
+            { Name: "Tools", Sales: 34, Margin: 20 },
+            { Name: "Electronics", Sales: 15, Margin: 25 },
+            { Name: "Gardening", Sales: 40, Margin: 5 }
+        ]);
+
     }
 
     private publish() {
