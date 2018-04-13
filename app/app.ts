de35ec8c2969426b74dbc317930a6189211a38e2
rakeshgirase <girase.rakesh@gmail.com>
2018-04-13 22:18:55 +0100

    QSB - Page Listener NPE Fix

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,7 @@ purpose of the file is to pass control to the app’s first module.
 */
 
 import * as app from 'application';
-import { isAndroid } from 'platform';
+import {isAndroid} from 'platform';
 import * as frame from 'ui/frame';
 
 require("./shared/firebase/firebase.common");
@@ -13,14 +13,14 @@ require("./shared/firebase/firebase.common");
 if (isAndroid) {
     app.android.on(app.AndroidApplication.activityBackPressedEvent, (args: app.AndroidActivityBackPressedEventData) => {
         const page = frame.topmost().currentPage;
-        if (page.hasListeners(app.AndroidApplication.activityBackPressedEvent)) {
+        if (page != null && page.hasListeners(app.AndroidApplication.activityBackPressedEvent)) {
             (<any>args).page = page;
             page.notify(args);
         }
     });
 }
 
-app.start({ moduleName: 'question/practice' });
+app.start({moduleName: 'question/practice'});
 
 /*
 Do not place any code after the application has been started as it will not
