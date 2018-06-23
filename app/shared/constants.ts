28c040e3f0bd017a1a251bc565661a8cfff30fea
rakeshgirase <girase.rakesh@gmail.com>
2018-06-23 23:20:16 +0100

    QSB - Admob URL Added

--- app/shared/constants.ts
+++ app/shared/constants.ts
@@ -1,2 +1,2 @@
-export const FIREBASE_URL = 'https://base-sasquiz.firebaseio.com/basesas';
+export const FIREBASE_URL = 'https://base-sasquiz.firebaseio.com/basesas/';
 //export const FIREBASE_URL = 'https://sas-quiz.firebaseio.com/';
