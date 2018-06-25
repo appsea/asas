9f6c0788de6e885bcf9b77aec651a038955c39d8
rakeshgirase <girase.rakesh@gmail.com>
2018-06-25 21:39:32 +0100

    QSB - ads.js

---
+++ app/services/ads.js
@@ -0,0 +1,60 @@
+var admobModule = require("nativescript-admob");
+let testing = false;
+
+exports.showFullBanner = function() {
+    this.createBanner(admobModule.AD_SIZE.FULL_BANNER);
+}
+
+exports.showLargeBanner = function() {
+    this.createBanner(admobModule.AD_SIZE.LARGE_BANNER);
+}
+
+exports.showLeaderboard = function() {
+    this.createBanner(admobModule.AD_SIZE.LEADERBOARD);
+}
+
+exports.showMediumRectangle = function() {
+    this.createBanner(admobModule.AD_SIZE.MEDIUM_RECTANGLE);
+}
+
+exports.showSmartBanner = function() {
+    this.createBanner(admobModule.AD_SIZE.SMART_BANNER);
+}
+
+exports.createBanner = function(size) {
+    admobModule.createBanner({
+        testing: testing,
+        size: size,
+        iosBannerId: "ca-app-pub-9082814869788754/7345143371",
+        androidBannerId: "ca-app-pub-9082814869788754/7345143371",
+        iosTestDeviceIds: ["yourTestDeviceUDIDs"],
+        margins: {
+            bottom: 0
+        }
+    }).then(function() {
+        console.log("admob createBanner done");
+    }, function(error) {
+        console.log("admob createBanner error: " + error);
+    });
+}
+
+exports.hideBanner = function() {
+    admobModule.hideBanner().then(function() {
+        console.log("admob hideBanner done");
+    }, function(error) {
+        console.log("admob hideBanner error: " + error);
+    });
+}
+
+exports.showInterstitial = function() {
+    admobModule.createInterstitial({
+        testing: testing,
+        iosInterstitialId: "ca-app-pub-9082814869788754/5195869671",
+        androidInterstitialId: "ca-app-pub-9082814869788754/5195869671",
+        iosTestDeviceIds: ["yourTestDeviceUDIDs"]
+    }).then(function() {
+        console.log("admob createInterstitial done");
+    }, function(error) {
+        console.log("admob createInterstitial error: " + error);
+    });
+}
