5f38be43d39fd71cd16c0cd4c7d0a93cfa22225f
rakeshgirase <girase.rakesh@gmail.com>
2018-06-27 23:19:43 +0100

    Link To Base SAS

--- app/shared/my-drawer/MyDrawer.ts
+++ app/shared/my-drawer/MyDrawer.ts
@@ -37,6 +37,11 @@ export function share(args: EventData): void {
         "Hi there, Take a look at the Base Sas Quiz which I am using for preparing For Base SAS Certification!!!");
 }
 
+export function goToAdvanceSas(args: EventData): void {
+    SocialShare.shareText("https://goo.gl/8aN3rG\n" +
+        "Hi there, Take a look at the Base Sas Quiz which I am using for preparing For Advance SAS Certification!!!");
+}
+
 export function logout(args: EventData): void {
     exit();
 }
